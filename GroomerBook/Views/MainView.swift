40a6ddb703070d9f3645a466fc0ff4bd10a9d64c
Chuck Belcher <cfbelcher@pateogroup.com>
2023-07-10 23:26:35 -0400

    Stubbed out tab view for navigation.

---
+++ GroomerBook/Views/MainView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  GroomerBook
+//
+//  Created by Chuck Belcher on 7/10/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            Text("Appointments View")
+                .tabItem {
+                    VStack {
+                        Label("Appointment", systemImage: "calendar")
+                    }
+                }
+            
+            Text("Customer View")
+                .tabItem {
+                    VStack {
+                        Label("Customers", systemImage: "person")
+                    }
+                }
+            
+            Text("Orders View")
+                .tabItem {
+                    VStack {
+                        Label("Orders", systemImage: "tray")
+                    }
+                }
+            Text("Reports View")
+                .tabItem {
+                    VStack {
+                        Label("Reports", systemImage: "calendar")
+                    }
+                }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
