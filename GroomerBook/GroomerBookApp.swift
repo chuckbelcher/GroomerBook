744be442c03b958cc72e904b15419c09a59bab3b
Chuck Belcher <cfbelcher@pateogroup.com>
2023-07-10 23:02:40 -0400

    Initial Commit

---
+++ GroomerBook/GroomerBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroomerBookApp.swift
+//  GroomerBook
+//
+//  Created by Chuck Belcher on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GroomerBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
